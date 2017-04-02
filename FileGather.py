53edad17d64713f8049f30499529eebae9a9aec7
Steven Jorgensen <spjorgensen94@gmail.com>
2017-04-02 08:37:49 -0700

    gathers urls and saves the html to files

--- FileGather.py
+++ FileGather.py
@@ -1,1 +1,52 @@
-import urllib.request
+import urllib
+
+from bs4 import BeautifulSoup
+
+filename = "html/test.html"
+cuisine = "chinese"
+main_url = "http://allrecipes.com/recipes/695/world-cuisine/asian/chinese/?page=1"
+
+try:local_filename, headers = urllib.request.urlretrieve(main_url)
+except:
+    print("\n### Unable to open webpage " + url + " ### \n")
+    exit(-1)
+
+url_file = open(local_filename)
+html = url_file.read()
+soup = BeautifulSoup(html, 'html.parser')
+
+div = soup.find_all('article', class_='grid-col--fixed-tiles')
+
+url_list = []
+
+for item in div:
+    for a in item.find_all('a', href=True):
+        if "/recipe" in a['href']:
+            if a['href'] not in url_list:
+                url_list.append(a['href'])
+                
+                
+url_file.close()
+
+filenum = 0
+for url in url_list:
+    urlname = "http://allrecipes.com" + url
+    html_filename = "html/" + cuisine +"/" + cuisine + str(filenum) + ".html"
+
+    html_file = open(html_filename, 'w')
+    print(urlname)
+    
+    try:local_filename, headers = urllib.request.urlretrieve(urlname)
+    except:
+        print("UNABLE TO OPEN " + urlname)
+        exit(-1)
+
+    file_ = open(local_filename)
+    data = file_.read()
+    
+
+    html_file.write(data)
+    html_file.close()
+    file_.close()
+    
+    filenum += 1
--- RecipeScraper.py
+++ RecipeScraper.py
@@ -9,11 +9,47 @@ args = parser.parse_args()
 
 url_file = open(str(args.url_file[0]))
 
+data_set = []
+
 for url in url_file:
+    url = url.strip()
     print(url)
 
 
-#try:local_filename, headers = urllib.request.urlretrieve(str(args.url[0]))
-#except:
- #   print("\n### Unable to open webpage ### \n")
-  #  exit(-1)
+    try:local_filename, headers = urllib.request.urlretrieve(url)
+    except:
+        print("\n### Unable to open webpage " + url + " ### \n")
+        exit(-1)
+
+    html = open(local_filename).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    ingredient_list = []
+    instruction_list = []
+
+    for ingredient in soup.find_all("span", class_="recipe-ingred_txt added"):
+        ingredient_list.append(ingredient.next)
+
+    for instruction in soup.find_all("span", class_="recipe-directions__list--item"):
+        instruction = str(instruction.text).replace('\n', '')
+
+        if instruction != "":
+            instruction_list.append(instruction)
+
+
+    punct = ['.', '?', '!', ',', ';', ':']
+
+    recipe_data = ""
+
+    for ingredient in ingredient_list:
+        words = ingredient.split()
+
+        for word in words:
+            if word[-1] in punct:
+                if word[-2] != " ":
+                    end = word[-1]
+                    word = word[:-1] + " " + end
+            
+            recipe_data += ( word + " " )
+
+    print(recipe_data)
