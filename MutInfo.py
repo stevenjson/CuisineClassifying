1c499c5f2daf641cb688e2a24956bcd04b271e9c
Steven Jorgensen <spjorgensen94@gmail.com>
2017-04-29 18:28:56 -0700

    Added mutual information script

---
+++ MutInfo.py
@@ -0,0 +1,134 @@
+import math
+import argparse
+
+def GetFile(fileName, path):
+    _file = open(path + fileName, 'r')
+    recipeList = _file.read().split('\n')
+    recipeList.pop()
+    _file.close()
+
+    return recipeList
+
+def Count(recipeMap, cuisineList, recipeList):
+    mutMap = {}
+
+    for word in recipeList:
+        for i in range(len(cuisineList)):
+            for recipe in recipeMap[cuisineList[i]]:
+                recipe = recipe.split(" ")
+
+                if word in recipe:
+                    if word in mutMap.keys():
+                        mutMap[word][i] += 1
+                    else:
+                        mutMap[word] = [0]*len(cuisineList)
+                        mutMap[word][i] += 1
+
+    return mutMap
+
+
+def MutInfo(word, countMap, foldSize, totalSize, recipeList, cuisineList):
+    probWord = 0
+    probClass = foldSize / float(totalSize)
+    probWordClass = 0
+    mutInfo = 0
+    wordTotal = 0
+
+    for item in countMap[word]:
+        wordTotal += item
+
+    probWord = wordTotal / float(totalSize)
+
+    for count in countMap[word]:
+        probWordClass = count / float(totalSize)
+
+        pointMut = 0.0
+
+        if probWordClass != 0:
+            pointMut = (math.log(probWordClass, 2)) + (math.log(probWord * probClass, 2) * -1)
+
+
+        mutInfo += (probWordClass * pointMut)
+
+    probWord = (totalSize - wordTotal) / float(totalSize)
+
+    for count in countMap[word]:
+        probWordClass = (foldSize - count) / float(totalSize)
+
+        pointMut = 0.0
+
+        if probWordClass != 0:
+            pointMut = (math.log(probWordClass, 2)) + (math.log(probWord * probClass, 2) * -1)
+
+
+        mutInfo += (probWordClass * pointMut)
+
+
+
+    return mutInfo
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("feature", type=str, help="Type of feature to use. ")
+    parser.add_argument("topN", type=int, help="Top N words to display")
+    args = parser.parse_args()
+
+    fileList = ["chinese.txt", "caribbean.txt", "french.txt", "italian.txt", "mexican.txt"]
+
+    feature = args.feature
+    
+    if feature != "none":
+        filePath = "Data/features/" + feature + "/"
+    else:
+        filePath = "Data/"
+
+    n = args.topN
+    foldSize = 180
+    totalSize = 900
+
+    cuisineList = []
+    recipeMap = {}
+    probMap = {}
+
+    for _file in fileList:
+        cuisine = _file.strip(".txt")
+        cuisineList.append(cuisine)
+        recipeList = GetFile(_file, filePath)
+        recipeMap[cuisine] = recipeList
+
+    recipeStr = ""
+    for cuisine in cuisineList:
+        for recipe in recipeMap[cuisine]:
+            recipeStr += (recipe + " ")
+
+    recipeList = set(recipeStr.split(" "))
+
+    countMap = Count(recipeMap, cuisineList, recipeList)
+
+    mutInfo = MutInfo("soy", countMap, foldSize, totalSize, recipeList, cuisineList)
+    
+    for word in recipeList:
+        if word == "" or  word == " ":
+            continue
+        probMap[word] = MutInfo(word, countMap, foldSize, totalSize, recipeList, cuisineList)
+
+    print()
+
+    for word in sorted(probMap, key=probMap.get, reverse=True)[:n]:
+        maxId = 0
+        maxCount = 0
+        for i in range(len(countMap[word])):
+            if countMap[word][i] > maxCount:
+                maxId = i
+                maxCount = countMap[word][i]
+        print("{:15s}: {:.5f}   {:10s}".format(word, probMap[word], cuisineList[maxId]))
+        print()
+        #print("Counts: {}".format(countMap[word]))
+        #print(cuisineList)
+        
+
+    #TopN(mutualMap, n)
+
+    pass
+
+main()
